886e5fd87a4d0f2c845c2d3c2484f5fe204ee283
JoseZunigaDyehs <zuniga.joseignacio@gmail.com>
2018-06-06 23:07:39 -0400

    HTML JS Recepción de productos

---
+++ portafolio/portafolio/Controllers/RecepcionProductoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace portafolio.Controllers
+{
+    public class RecepcionProductoController : Controller
+    {
+        // GET: RecepcionProducto
+        public ActionResult Index()
+        {
+            if (Session["usuario"] != null)
+            {
+                return View();
+            }
+            return Redirect("~/Login/");
+        }
+    }
+}
